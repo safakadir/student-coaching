b5fb1edb021d5faf32c17637395ee8a104e1016b
safakadir <safakadir@gmail.com>
2023-03-05 10:01:42 +0300

    navbar formed

--- components/nav-item.tsx
+++ components/nav-item.tsx
@@ -1,12 +1,21 @@
+import { ReactElement } from "react"
+
 interface NavItemProps {
     icon?: string,
-    title: string
+    title: string,
+    children?: ReactElement<NavItemProps> | ReactElement<NavItemProps>[],
+    noclick?: boolean
 }
 
-const NavItem: React.FC<NavItemProps> = ({icon, title}) => {
-    return <li className="flex px-5 py-2 text-base hover:bg-gray-100 cursor-pointer">
-        <div className="pr-3">IC</div>
-        <div className="hidden md:block">{title}</div>
+const NavItem: React.FC<NavItemProps> = ({icon, title, children, noclick=false}) => {
+    return <li>
+        <div className={`flex px-5 py-2 text-base ${!noclick ? 'hover:bg-gray-100 cursor-pointer' : ''}`}>
+            <div className="pr-3">IC</div>
+            <div className="hidden md:block">{title}</div>
+        </div>
+        {children && 
+            <ul className="pl-6">{children}</ul>
+        }
     </li>
 }
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,17 +4,25 @@ import NavItem from "./nav-item"
 
 const Navbar: React.FC = ()  => {
     return <nav className="h-screen border w-16 md:w-72 xl:w-80">
-        <div className="h-16 px-2 md:px-4 flex items-center mb-5 border-bottom">
+        
+        <div className="h-16 px-2 md:px-4 flex items-center mb-6 border-bottom">
             <div className="relative w-11 h-11 lg:w-12 lg:h-12">
-                <Image className="" src="/logo.png" fill alt="logo" />
+                <Image src="/logo.png" fill alt="logo" />
             </div>
             <div className="hidden md:block pl-2">
                 <div className="lg:text-lg xl:text-xl font-bold">AREL AKADEMİ</div>
-                <div className="text-xs xl:text-sm text-neutral-500">Koçluk Bilgi Sistemi</div>
+                <div className="text-xs xl:text-sm">Koçluk Bilgi Sistemi</div>
             </div>
         </div>
+
         <ul>
             <NavItem title="Öğrenci İşlemleri" />
+            <NavItem title="Ahmet Çizmeci" noclick>
+                <NavItem title="Kaynak Takibi" />
+                <NavItem title="Deneme Analizi" />
+                <NavItem title="Görüşmeler" />
+                <NavItem title="Ödemeler" />
+            </NavItem>
             <NavItem title="Görüşme Takvimi" />
             <NavItem title="Tüm Ödemeler" />
         </ul>
