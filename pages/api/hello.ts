0d7aefffcbc1e9d4b1913146c5d8a6f7874b4162
safakadir <safakadir@gmail.com>
2023-03-06 08:27:21 +0300

    added edge function as sandbox playing

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const config = {
+  runtime: 'edge'
+}
+
 type Data = {
-  name: string
+  name: string,
+  runtime: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe', runtime: 'Edge' })
 }
