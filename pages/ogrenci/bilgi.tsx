6ad3a79e074604d2a8d73ac69ce15c1c348c1bdf
safakadir <safakadir@gmail.com>
2023-03-06 22:07:26 +0300

    bilgi page draft

--- pages/ogrenci/bilgi.tsx
+++ pages/ogrenci/bilgi.tsx
@@ -8,7 +8,13 @@ const OgrenciBilgiPage: React.FC = () => {
 
     return <Layout>
         Öğrenci Bilgi Sayfası
-        <h1 className="text-2xl font-bold">{selectedOgrenci?.fullname}</h1>
+        <h1 className="text-2xl font-bold my-7">{selectedOgrenci?.fullname}</h1>
+        <h3 className="text-base my-2"><span className="font-bold">Öğrenci İletişim:</span> {selectedOgrenci?.contactPhone}</h3>
+        <h3 className="text-base my-2"><span className="font-bold">Doğum Günü:</span> {selectedOgrenci?.birthDate}</h3>
+        <h3 className="text-base my-2"><span className="font-bold">Okul:</span> {selectedOgrenci?.term.school}</h3>
+        <h2 className="text-xl my-4"><span className="font-bold">Güncel Dönem:</span> {selectedOgrenci?.term.key}</h2>
+        <h2 className="text-xl my-4"><span className="font-bold">Sınıf:</span> {selectedOgrenci?.term.grade}</h2>
+        <h2 className="text-xl my-4"><span className="font-bold">Alan:</span> {selectedOgrenci?.term.field}</h2>
     </Layout>
 }
 
