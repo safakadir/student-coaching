c7a7ff4cadf69689380bed8f26d2c3de1ad1db8f
safakadir <safakadir@gmail.com>
2023-03-06 08:40:07 +0300

    another more experimental adjustment for edge function

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,18 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-  runtime: 'edge'
-}
+  runtime: 'edge',
+};
 
-type Data = {
-  name: string,
-  runtime: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.json({ name: 'John Doe', runtime: 'Edge' })
-}
+export default (req: NextRequest) => {
+  return NextResponse.json({
+    name: `Hello, from ${req.url} I'm now an Edge Function!`,
+  });
+};
